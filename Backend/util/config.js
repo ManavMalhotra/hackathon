0d1b83916c6ba00e193e1b6b43b4116b8fa4ae4d
ManavMalhotra <manavmalhotrafrnd4u@gmail.com>
2023-01-08 11:09:33 +0530

    Added openAi support

--- Backend/util/config.js
+++ Backend/util/config.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
 const PORT = process.env.PORT
 const MONGODB_URI = process.env.MONGODB_URI
+const apiKey = process.env.OPENAI
 
 module.exports = {
   MONGODB_URI,
-  PORT
+  PORT,
+  apiKey
 }
